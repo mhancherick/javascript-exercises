9cf67ecc4098f0ec2e2eff874eac8927b009921c
mhancherick <141765650+mhancherick@users.noreply.github.com>
2024-03-27 21:04:17 -0400

    completing javascript exercises through exercise 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
+    let sum = 0;
+
+    if(!Number.isInteger(number1) || !Number.isInteger(number2)){
+        return "ERROR";
+    }
+    else if(number1 < 0 || number2 < 0){
+        return "ERROR";
+    }
+    else {
+        if(number1 > number2) {
+            let x = number1;
+            number1 = number2;
+            number2 = x;
+        }
+        for (let index = 0; index < number2 - number1 + 1; index++) {
+            
+            sum = sum + number1 + index;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
