ff9158a8404546cb35fe463d13b570c5079c0211
mhancherick <141765650+mhancherick@users.noreply.github.com>
2024-05-01 19:35:19 -0400

    finishing object exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    let fib1 = 1;
+    let fib2 = 1;
+    let sumFib;
+    if(num < 0){
+        return "OOPS";
+    }
+    else if(num == 0){
+        return 0;
+    }
+    else if(num < 3){
+        return 1;
+    }
+    else {
+        for (let index = 0; index < num - 2; index++) {
+            sumFib = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = sumFib;          
+        }
+        return sumFib;
+    }
+    
 };
 
 // Do not edit below this line
